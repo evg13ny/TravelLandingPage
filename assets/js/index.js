ddfddd24a31c6fc315362189ab53fbb8e5ca327e
Evgeny Polyakov <evg_pol@yahoo.com>
2022-09-18 01:22:08 +0300

    added header

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+/* Show Menu */
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Hidden menu */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* Remove Menu Modile */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
