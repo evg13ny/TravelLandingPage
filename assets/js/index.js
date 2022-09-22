65c82cc91eda2a3196fe661311a15d07af8e57f8
Evgeny Polyakov <evg_pol@yahoo.com>
2022-09-22 23:44:37 +0300

    added Scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ if (navToggle) {
     })
 }
 
-/* Hidden menu */
+/* Hide menu */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
@@ -18,9 +18,11 @@ if (navClose) {
 
 /* Remove Menu Modile */
 const navLink = document.querySelectorAll('.nav__link');
+
 function linkAction() {
     navMenu.classList.remove('show-menu');
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /* Change Background Header */
@@ -55,6 +57,7 @@ const videoButton = document.getElementById('video-button');
 const videoIcon = document.getElementById('video-icon');
 
 function playPause() {
+
     if (videoFile.paused) {
         videoFile.play();
         videoIcon.classList.remove('ri-play-line');
@@ -75,3 +78,37 @@ function finalVideo() {
 
 // ended, when video ends
 videoFile.addEventListener('ended', finalVideo);
+
+/* Show Scroll Up */
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 200) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/* Scroll Section Active Link */
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
